1715d49bbbc30e31bbe6315858b2c2e428296b6b
NS-1559 <sloli.sonnguyen@gmail.com>
2023-01-02 22:47:55 +0700

    request block
    - ui for block request
    - ui for handle request
    - send request

--- src/js/page-news.js
+++ src/js/page-news.js
@@ -6,19 +6,19 @@
 
 function resizer() {
     var el = document.getElementById('blocked-container');
-    el.style.left = ((window.innerWidth  - el.offsetWidth)  / 2) + 'px';
+    el.style.left = ((window.innerWidth - el.offsetWidth) / 2) + 'px';
     el.style.top = ((window.innerHeight - el.offsetHeight) / 2) + 'px';
 }
 
-window.onload = function() {
+window.onload = function () {
     ls.set('_read_news', ENV_NEWS_VERSION);
     Controller.sharePage();
-    i18n(function() {
+    i18n(function () {
         document.getElementById('blocked-container').style.display = 'block';
         resizer();
     });
 };
 
-window.onresize = function() {
+window.onresize = function () {
     resizer();
 };
